00d58b69bdc18e9ab68b590b49acd457b0d96759
Haydee Delgado <dhelgado723@gmail.com>
2026-04-25 00:23:22 -0500

    commit RegistraSet

--- Problema1.py
+++ Problema1.py
@@ -7,3 +7,19 @@ class Equipo:
         self.setGanados = setGanados
 equipo1 = Equipo("Leones")
 equipo2 = Equipo("Águilas")
+
+def RegistraSet(equipo_ganador):
+    if equipo_ganador == 1:
+        equipo1.setGanados += 1
+        if equipo1.setGanados == 3:
+            equipo1.partidosGanados += 1
+            equipo2.partidosPerdidos += 1
+            equipo1.setGanados = 0
+            equipo2.setGanados = 0
+    else:
+        equipo2.setGanados += 1
+        if equipo2.setGanados == 3:
+            equipo2.partidosGanados += 1
+            equipo1.partidosPerdidos += 1
+            equipo1.setGanados = 0
+            equipo2.setGanados = 0 
